dc2f1cff32265221232936f88c99dc531f0f687b
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2022-12-12 12:35:55 +0300

    some updates

--- pyworkforce/plotters/matplotlib.py
+++ pyworkforce/plotters/matplotlib.py
@@ -3,7 +3,7 @@ import operator
 import numpy as np
 #! /usr/bin/env python
 
-def plot(solution, shifts_spec, num_days, img_filename=None,resource_height=1.0,show_task_labels=True,
+def plot(solution, shifts_spec, num_days, shift_colors, img_filename=None,resource_height=1.0,show_task_labels=True,
          color_prec_groups=False,hide_tasks=[],hide_resources=[],task_colors=dict(),fig_size=(15,5),
      vertical_text=False) :
     """
@@ -27,7 +27,14 @@ def plot(solution, shifts_spec, num_days, img_filename=None,resource_height=1.0,
     shifted_hours = solution['shifted_hours']
 
     fig, ax = plt.subplots(1, 1, figsize=fig_size)
-   
+
+    # colors = {
+    #   "Morning8":'#7EA7D8',
+    #   'Morning12': '#FFF79A',
+    #   'Afternoon': '#A1D372',
+    #   'Night':'#EB4845',
+    #   'Mixed':'#7BCDC8'
+    # }
 
 
     plt.ylim(0, solution['total_resources'])
@@ -39,6 +46,7 @@ def plot(solution, shifts_spec, num_days, img_filename=None,resource_height=1.0,
         id = i['id']
         day = i['day']
         shift_hours = shifts_spec[i['shift']]
+        # print(i['shift'])
 
         for idx, j in enumerate(shift_hours):
             if(j > 0):
@@ -47,7 +55,7 @@ def plot(solution, shifts_spec, num_days, img_filename=None,resource_height=1.0,
                     (day * 24 + idx, id),       # (x,y)
                     1,   # width (1h)
                     resource_height,   # height
-                    color = '#A1D372',
+                    color = shift_colors[i['shift']], #'#A1D372',
                     alpha=0.6
                     )
         )
@@ -57,15 +65,15 @@ def plot(solution, shifts_spec, num_days, img_filename=None,resource_height=1.0,
 
     # [for i['resource'] in solution['resource_shifts']]
     all_res = list(map(lambda x: {'id': x['id'], 'resource': x['resource']}, solution['resource_shifts']))
-    
+
     uniq_res = list({v['id']:v for v in all_res}.values())
-    
-    plt.yticks([x + resource_height/2.0 for x in range(len(uniq_res))], [i['resource'] for i in uniq_res]) 
+
+    plt.yticks([x + resource_height/2.0 for x in range(len(uniq_res))], [i['resource'] for i in uniq_res])
 
     plt.xticks(np.arange(0, num_days * 24 + 1, 12))
 
     plt.title(f'Employee Scheduling: {len(uniq_res)} employees, {num_days} days ({num_days * 24} hours)')
-    
+
     if img_filename is not None:
         fig.figsize=(1,1)
         plt.savefig(img_filename,dpi=200,bbox_inches='tight')
