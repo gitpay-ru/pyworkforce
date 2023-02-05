fba04ee265c1a15962078c5a2d8e157283d38f0b
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2023-02-05 21:48:46 +0300

    init minWorkingHours

--- examples/staffing/planner.py
+++ examples/staffing/planner.py
@@ -3,35 +3,17 @@ import sys
 path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 
-from pyworkforce.scheduling import MinAbsDifference
-from pyworkforce.queuing import ErlangC
-from pprint import PrettyPrinter
 import pandas as pd
-import math
 import json
-import numpy as np
-from collections import deque
-
-from pyworkforce.plotters.scheduling import plot, plot_xy_per_interval
-from pyworkforce.utils.shift_spec import get_shift_coverage, get_shift_colors, decode_shift_spec, unwrap_shift
-from pyworkforce.utils.common import get_datetime
+import argparse
 from pyworkforce.staffing import MultiZonePlanner
-import pytz
-eastern = pytz.timezone('US/Eastern')
 
+# input_csv_path = '../scheduling_input.csv'
+# input_meta_path = '../scheduling_meta_input.json'
+# output_dir = '../out'
 
-input_csv_path = '../scheduling_input.csv'
-input_meta_path = '../scheduling_meta_input.json'
-output_dir = '../out'
-
-Path(output_dir).mkdir(parents=True, exist_ok=True)
-df = pd.read_csv(input_csv_path, parse_dates=[0], index_col=0)
-
-with open(input_meta_path, 'r', encoding='utf-8') as f:
-    meta = json.load(f)
-
-mzp = MultiZonePlanner(df, meta, output_dir)
-mzp.solve()
+# mzp = MultiZonePlanner(df, meta, output_dir)
+# mzp.solve()
 
 # mzp.schedule()
 # mzp.roster()
@@ -40,3 +22,42 @@ mzp.solve()
 # mzp.combine_results()
 # mzp.recalculate_stats()
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process csv and meta.')
+    parser.add_argument('-i', '--input', default='../scheduling_input.csv')
+    parser.add_argument('-m', '--meta', default='../scheduling_meta_input.json')
+    parser.add_argument('-o', '--out', default='../out')
+    parser.add_argument('-c', '--calculate', default='all')
+    return parser.parse_args()
+
+def do_planning(args):
+    print(args.input, args.meta, args.out)
+
+    Path(args.out).mkdir(parents=True, exist_ok=True)
+    df = pd.read_csv(args.input, parse_dates=[0], index_col=0)
+
+    with open(args.meta, 'r', encoding='utf-8') as f:
+        meta = json.load(f)
+
+    mzp = MultiZonePlanner(df, meta, args.out)
+    
+    if(args.calculate == 's'):
+        mzp.schedule()
+    elif(args.calculate == 'sr'):
+        mzp.schedule()
+        mzp.roster()
+    elif(args.calculate == 'srbpc'):
+        mzp.schedule()
+        mzp.roster()
+        mzp.roster_breaks()
+        mzp.roster_postprocess()
+        mzp.combine_results()
+    else:
+        mzp.solve()
+
+def main():
+    args = parse_args()
+    do_planning(args)
+
+if __name__ == '__main__':
+    main()
