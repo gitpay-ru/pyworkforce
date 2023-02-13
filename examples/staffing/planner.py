fa78f271a97d8fcc778493f5321a11e9f59e3ac5
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2023-02-13 12:26:40 +0300

    clean up

--- examples/staffing/planner.py
+++ examples/staffing/planner.py
@@ -1,23 +1,14 @@
 from pathlib import Path
 import sys
 
-from pyworkforce.solver_params import SolverParams
-
 path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 
-from pyworkforce.scheduling import MinAbsDifference
-from pyworkforce.queuing import ErlangC
-from pprint import PrettyPrinter
-import pandas as pd
-import math
-import json
-import numpy as np
-from collections import deque
+from pyworkforce.solver_params import SolverParams
 
-from pyworkforce.plotters.scheduling import plot, plot_xy_per_interval
-from pyworkforce.utils.shift_spec import get_shift_coverage, get_shift_colors, decode_shift_spec, unwrap_shift
-from pyworkforce.utils.common import get_datetime
+import pandas as pd
+import json
+
 from pyworkforce.staffing import MultiZonePlanner
 import pytz
 eastern = pytz.timezone('US/Eastern')
@@ -25,7 +16,7 @@ eastern = pytz.timezone('US/Eastern')
 
 input_csv_path = '../data_file.csv'
 input_meta_path = '../meta_file.json'
-output_dir = '../out'
+output_dir = '../out2'
 
 Path(output_dir).mkdir(parents=True, exist_ok=True)
 df = pd.read_csv(input_csv_path, parse_dates=[0], index_col=0)
